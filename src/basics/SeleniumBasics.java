039ead81a4086d5deb7eaef0721156b22346e5a1
unknown <mandyatwork24@gmail.com>
2024-08-23 20:04:24 +0530

    added

---
+++ src/basics/SeleniumBasics.java
@@ -0,0 +1,35 @@
+package basics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumBasics {
+
+	public static void main(String[] args) {
+
+		// ctrl+shift+o
+		WebDriver driver = new ChromeDriver();
+        
+		//To maximize window
+		driver.manage().window().maximize();
+		
+		//To open browser
+		driver.get("https://www.google.com");
+
+		//To Grab Title of page
+		String title = driver.getTitle();
+		System.out.println(title);
+		
+		//grab current url that is opened
+		String URL=driver.getCurrentUrl();
+		System.out.println(URL);
+		
+		//is closing browser
+		driver.close();
+		
+		
+		
+
+	}
+
+}
